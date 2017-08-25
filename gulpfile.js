d21bb4066b736e289b47f55f63bf7e825db896aa
Wallison <franciscowallison@gmail.com>
2017-08-25 09:32:57 -0300

    Configuração do webpack em gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 const elixir = require('laravel-elixir');
+const webpackConfig = require('./webpack.config');
+const webpackDevConfig = require('./webpack.dev.config');
+
 
 require('laravel-elixir-vue');
+require('laravel-elixir-webpack-official');
+
+Elixir.webpack.config.module.loaders = [];
+
+Elixir.webpack.mergeConfig(webpackConfig);
+Elixir.webpack.mergeConfig(webpackDevConfig);
+
 
 /*
  |--------------------------------------------------------------------------
@@ -13,13 +23,13 @@ require('laravel-elixir-vue');
  |
  */
 
-elixir((mix) => {
-    mix.sass('./resources/assets/admin/sass/admin.scss')
-    	.copy('./node_modules/materialize-css/fonts/roboto','./public/fonts/roboto');
-       //.webpack('app.js');
-    //ficar onhado os aquivos blade e publica
-    mix.browserSync({
-    	host: '0.0.0.0',
-    	proxy: 'http://192.168.1.2:8000'
-    });
-});
+// elixir((mix) => {
+//     mix.sass('./resources/assets/admin/sass/admin.scss')
+//     	.copy('./node_modules/materialize-css/fonts/roboto','./public/fonts/roboto');
+//        //.webpack('app.js');
+//     //ficar onhado os aquivos blade e publica
+//     mix.browserSync({
+//     	host: '0.0.0.0',
+//     	proxy: 'http://192.168.1.2:8000'
+//     });
+// });
