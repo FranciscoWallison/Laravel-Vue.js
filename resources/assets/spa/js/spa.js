d1be64d1e76ee9d70873c930117bfed49d61803e
Wallison <franciscowallison@gmail.com>
2017-08-29 10:24:50 -0300

    Local Storage (Exemplos de uso)

--- resources/assets/spa/js/spa.js
+++ resources/assets/spa/js/spa.js
@@ -9,6 +9,12 @@ Vue.http.options.root = "http://192.168.1.2:8000/api";
 
 require('./router');
 
+// window.localStorage.setItem('name', 'values');
+// window.localStorage.setItem('name1', 'alterado');
+// window.localStorage.removeItem('name');
+// window.localStorage.clear();
+
+
 // import Echo from "laravel-echo"
 
 // window.Echo = new Echo({
