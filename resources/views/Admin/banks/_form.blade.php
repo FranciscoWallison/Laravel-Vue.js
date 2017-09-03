03d4d31520587760e041f2fd53ac78783c6d5c74
Wallison <franciscowallison@gmail.com>
2017-09-03 16:37:16 -0300

    Mostrando mensagens de erro

--- resources/views/Admin/banks/_form.blade.php
+++ resources/views/Admin/banks/_form.blade.php
@@ -1,6 +1,10 @@
+@include('errors._error_field')
+@php
+	$errorsClass = $errors->first('name') ? ['class' => 'validate invalid'] : []; 
+@endphp
 <div class="row">
-	<div class="input-field col s6">
-		{!! Form::label('name', 'Nome') !!}
-		{!! Form::text('name', null) !!}
+	<div class="input-field col s12">
+		{!! Form::text('name', null, $errorsClass) !!}
+		{!! Form::label('name', 'Nome' , ['data-error' => $errors->first('name') ]) !!}
 	</div>
 </div>
--- resources/views/Admin/banks/create.blade.php
+++ resources/views/Admin/banks/create.blade.php
@@ -1,17 +1,30 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container">
-    <div class="row">
-    	<h4>Novo Banco</h4>
-        {!! Form::open(['route' => 'admin.banks.store' ] ) !!}
-			
-			@include('admin.banks._form')
 
-			<div class="row">
-				{!! Form::submit('Criar banco', ['class' => 'btn waves-effect']) !!}
-			</div>
-        {!! Form::close() !!}
-    </div>
-</div>
+	<div class="container">
+	    <div class="row ">
+	    	<div class="col s12 responsive-table z-depth-5">
+	    		<div class="container">
+			    	<h4 class="center">Novo Banco</h4>
+			        {!! Form::open(['route' => 'admin.banks.store' ] ) !!}
+
+						<div class="row">
+	       					<div class="input-field">
+								@include('admin.banks._form')
+							</div>
+						</div>
+						
+						<div class="row">
+							{!! Form::submit('Criar banco', ['class' => 'waves-effect waves-light btn']) !!}
+						</div>
+						
+						
+			        {!! Form::close() !!}
+			    </div>
+	    	</div>
+	    </div>
+	</div>
+
+
 @endsection
