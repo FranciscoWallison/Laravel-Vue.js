baedda12effe65b2cc209313457feaab3457a277
Wallison <franciscowallison@gmail.com>
2017-09-03 16:42:30 -0300

    Criando campo para upload de logo

--- resources/views/Admin/banks/_form.blade.php
+++ resources/views/Admin/banks/_form.blade.php
@@ -7,4 +7,13 @@
 		{!! Form::text('name', null, $errorsClass) !!}
 		{!! Form::label('name', 'Nome' , ['data-error' => $errors->first('name') ]) !!}
 	</div>
+	<div class="input-field file-field col s6">
+		<div class="btn">
+			<span>Logo</span>
+			{!! Form::file('logo') !!}
+		</div>
+		<div class="file-path-wrapper">
+			<input type="text" class="file-path">
+		</div>
+	</div>
 </div>
