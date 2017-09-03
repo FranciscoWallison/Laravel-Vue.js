fb8d41113172651791e32f96483664adfbd7d5d4
Wallison <franciscowallison@gmail.com>
2017-09-03 14:26:41 -0300

    Exclusão modal

--- resources/assets/admin/js/admin.js
+++ resources/assets/admin/js/admin.js
@@ -16,6 +16,7 @@ require('./bootstrap');
 Vue.component('example', require('./components/Example.vue'));
 Vue.component('admin-menu', require('./components/AdminMenu.vue'));
 Vue.component('delete-action', require('./components/DeleteAction.vue'));
+Vue.component('modal', require('../../_default/components/Modal.vue'));
 
 
 
