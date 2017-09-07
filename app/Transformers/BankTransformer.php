5ef4e43ca1239582eccb7dd1e589e28825e02e68
Wallison <franciscowallison@gmail.com>
2017-09-07 16:38:19 -0300

    Serializando link para logo do banco

--- app/Transformers/BankTransformer.php
+++ app/Transformers/BankTransformer.php
@@ -23,11 +23,20 @@ class BankTransformer extends TransformerAbstract
         return [
             'id'         => (int) $model->id,
             'name'       => $model->name,
+            'logo'       => $this->makeLogoPath($model),
 
             /* place your other model properties here */
 
             'created_at' => $model->created_at,
             'updated_at' => $model->updated_at
         ];
+    }
+
+
+    public function makeLogoPath(Bank $model)
+    {
+        $url    = url('/'); // locahost::8080
+        $folder = Bank::logosDir();
+        return "$url/storage/$folder/{$model->logo}";
     }
 }
