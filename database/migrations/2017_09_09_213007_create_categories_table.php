cee25cf086262b1b3e65144f4f0915099f9b7906
Wallison <franciscowallison@gmail.com>
2017-09-09 18:45:27 -0300

    Criando tabela de categorias

---
+++ database/migrations/2017_09_09_213007_create_categories_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Kalnoy\Nestedset\NestedSet;
+
+class CreateCategoriesTable extends Migration
+{
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('categories', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');// left right parent_id
+
+            //Campos NESTED SET MODEL
+            NestedSet::columns($table);
+
+            // Relacionamento multitenancy
+            $table->integer('client_id')->unsigned();
+            $table->foreign('client_id')->references('id')->on('clients');
+            // DB::select('DESCRIBE categories')
+            $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('categories');
+	}
+
+}
