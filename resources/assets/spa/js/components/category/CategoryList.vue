2e912f52e2b2a6264dd4734c4d1b737dd5fb5aeb
Wallison <franciscowallison@gmail.com>
2017-09-09 22:51:11 -0300

    Listando categorias pais e filhas

--- resources/assets/spa/js/components/category/CategoryList.vue
+++ resources/assets/spa/js/components/category/CategoryList.vue
@@ -5,7 +5,7 @@
 				<h5>Administração de categoria</h5>
 			</page-title>
             <div class="card-panel z-depth-5">
-               
+               <category-tree :categories="categories"></category-tree>
             </div>
 
 		</div>
@@ -13,10 +13,30 @@
 </template>
 <script>
 	import PageTitleComponent from '../../../../_default/components/PageTitle.vue';
+	import CategoryTreeComponent from './CategoryTree.vue';
+	import {Category} from '../../services/resources';
+
+
 	export default {
 
     	components: {    		
-            'page-title': PageTitleComponent,        
+            'page-title': PageTitleComponent,
+            'category-tree': CategoryTreeComponent
+    	},
+    	data(){
+    		return{
+    			categories:[]
+    		}
+    	},
+    	created(){
+    		this.getCategories();
+    	},
+    	methods: {
+    		getCategories(){
+    			Category.query().then(response => {
+    				this.categories = response.data.data;
+    			})
+    		}
     	}
 	}
 	
---
+++ resources/assets/spa/js/components/category/CategoryTree.vue
@@ -0,0 +1,21 @@
+<template>
+	<ul>
+		<li v-for="(index, o) in categories">
+			{{ o.name }}
+			<category-tree :categories="o.children.data"></category-tree>	
+		</li>
+	</ul>
+</template>
+<script>
+
+	export default {
+		name: 'category-tree',
+		props:{
+			categories: {
+				type: Array,
+				required: true
+			}
+		}
+	}
+	
+</script>
