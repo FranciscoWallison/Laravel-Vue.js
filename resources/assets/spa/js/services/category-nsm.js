88ce67bf4e1ac775229b800171ca44d3c62cf2b5
Wallison <franciscowallison@gmail.com>
2017-09-10 15:26:24 -0300

    Criando lógica para edição de categorias

--- resources/assets/spa/js/services/category-nsm.js
+++ resources/assets/spa/js/services/category-nsm.js
@@ -9,7 +9,7 @@ export class CategoryFormat{
 			level: 0,
 			hasChildren: false  
 		});
-console.log(categoriesFormatted);
+//console.log(categoriesFormatted);
 		return categoriesFormatted;
 	}
 
@@ -48,5 +48,68 @@ export class CategoryService{
 
 			return reponse;
 		})
+	}
+
+	static edit(category, parent, categories, categoriesOriginal){
+
+		let categoryCopy = $.extend(true, {}, category);
+		if(categoryCopy.parent_id === null){
+			delete categoryCopy.parent_id;//excluir para não mecher no objeto original
+		}
+		let self = this; //pega o contexto da classe
+		return Category.update({id: categoryCopy.id}, categoryCopy).then(reponse => {
+
+			let categoryUpdated = reponse.data.data;
+			//verifica se é mestre ou filha
+			if(categoryUpdated.parent_id === null){
+				/*
+				* Categoria alterada, está sem pai
+				* E antes ela tinha um pai
+				*/
+				if(parent){
+					parent.children.data.$remove(categoriesOriginal);
+					categories.push(categoryUpdated);
+					return reponse;
+				}
+			}else{
+				/*
+				* Categoria alterada, está tem pai
+				*/
+				if(parent){
+					/*
+					* Trocar categoria de pai
+					*/
+					if(parent.id != categoryUpdated.parent_id){
+						parent.children.data.$remove(categoriesOriginal);
+						self._addChild(categoryUpdated, categories);
+						return reponse;
+					}
+				}else{
+					/*
+					* Trocar categoria mestre um filho
+					* Antes a categoria era um pai
+					*/
+					categories.$remove(categoriesOriginal);
+					self._addChild(categoryUpdated, categories);
+					return reponse;
+				}
+			}
+
+			return reponse;
+		});
+	}
+
+	static _addChild(child, categories){
+		let parent = this._findParent(child.parent_id, categories);
+		parent.children.data.push(child);//adinconado filho
+	}
+
+	static _findParent(id, categories){
+		for(let category of categories){
+			if(id == category.id){
+				return category; //
+			}
+			return this._findParent(id , category.children.data);
+		}
 	}
 }
