3a732152acd1b31a37124f20b00c5dd8e9616511
Wallison <franciscowallison@gmail.com>
2017-09-13 00:25:06 -0300

    Store para conta a pagar

--- resources/assets/spa/js/services/resources.js
+++ resources/assets/spa/js/services/resources.js
@@ -21,6 +21,7 @@ let Bank = Vue.resource('banks');
 let BankAccount = Vue.resource('bank_accounts{/id}'); // com o vue resource temos todas as operações put. delete update etc
 let CategoryRevenue = Vue.resource('category_revenues{/id}');
 let CategoryExpense = Vue.resource('category_expenses{/id}');
+let BillPay = Vue.resource('bill_pays{/id}');
 
-export {User, BankAccount, Bank, CategoryRevenue, CategoryExpense};
+export {User, BankAccount, Bank, CategoryRevenue, CategoryExpense, BillPay};
 
