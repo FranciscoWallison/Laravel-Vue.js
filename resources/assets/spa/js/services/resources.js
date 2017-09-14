efccc1a7afe8cf404f1bfb0eb2e6f6b427d2e3ce
Wallison <franciscowallison@gmail.com>
2017-09-13 21:54:43 -0300

    Metódo personalizado para listar contas bancárias

--- resources/assets/spa/js/services/resources.js
+++ resources/assets/spa/js/services/resources.js
@@ -18,7 +18,9 @@ export class Jwt{
 
 let User = Vue.resource('user');
 let Bank = Vue.resource('banks'); 
-let BankAccount = Vue.resource('bank_accounts{/id}'); // com o vue resource temos todas as operações put. delete update etc
+let BankAccount = Vue.resource('bank_accounts{/id}', {}, {
+	lists: {method: 'GET', utl: 'bank_accounts/lists'}
+}); // com o vue resource temos todas as operações put. delete update etc
 let CategoryRevenue = Vue.resource('category_revenues{/id}');
 let CategoryExpense = Vue.resource('category_expenses{/id}');
 let BillPay = Vue.resource('bill_pays{/id}');
--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,8 @@ Route::group(['middleware' => 'cors' , 'as' => 'api.'], function(){
 
 	Route::group(['middleware' => 'auth:api'], function(){
 		Route::resource('banks', 'Api\BanksController', ['only' => ['index']]);
+
+		Route::resource('bank_accounts/lists', 'Api\BankAccountsController@lists')->name('bank_accounts.lists');
 		Route::resource('bank_accounts', 'Api\BankAccountsController', ['except'=> ['create' , 'edit'] ]);
 
 		Route::resource('category_expenses', 'Api\CategoryExpensesController', ['except'=> ['create' , 'edit'] ]);
