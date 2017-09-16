9fd7c6b8b0d3da550c5e0cce2f27053a3713d129
Wallison <franciscowallison@gmail.com>
2017-09-16 15:18:23 -0300

    Saldo da conta bancária

--- app/Repositories/BankAccountRepositoryEloquent.php
+++ app/Repositories/BankAccountRepositoryEloquent.php
@@ -2,11 +2,14 @@
 
 namespace CodeFin\Repositories;
 
-use Prettus\Repository\Eloquent\BaseRepository;
-use Prettus\Repository\Criteria\RequestCriteria;
-use CodeFin\Repositories\BankAccountRepository;
+use CodeFin\Criteria\LockTableCriteria;
+// use CodeFin\Events\BankAccountBalanceUpdatedEvent;
 use CodeFin\Models\BankAccount;
 use CodeFin\Presenters\BankAccountPresenter;
+use CodeFin\Repositories\Interfaces\BankAccountRepository;
+use Prettus\Repository\Criteria\RequestCriteria;
+use Prettus\Repository\Eloquent\BaseRepository;
+
 
 /**
  * Class BankAccountRepositoryEloquent
@@ -22,6 +25,27 @@ class BankAccountRepositoryEloquent extends BaseRepository implements BankAccoun
         'bank.name' => 'LIKE'
     ];
 
+    /*
+    * Evidando trançãçoes concorrentes
+    */
+    public function addBalance($id, $value)
+    {
+        $skipPresenter = $this->skipPresenter;
+        $this->skipPresenter(true);
+
+        \DB::beginTransaction();//block table
+        $this->pushCriteria(new LockTableCriteria());
+            $model = $this->find($id);
+            $model->balance = $model->balance + $value;
+            $model->save();
+        \DB::commit();
+        // broadcast(new BankAccountBalanceUpdatedEvent($model));
+        $this->popCriteria(LockTableCriteria::class); //libera a tabela
+
+        $this->skipPresenter = $skipPresenter;
+        return $this->parserResult($model);
+            return $model;
+    }
     /**
      * Specify Model class name
      *
