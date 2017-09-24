b3170e454e2ef3819a6fa4196b8c6dffb00f1d6c
Wallison <franciscowallison@gmail.com>
2017-09-24 11:55:34 -0300

    Integração com iugu-php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -39,5 +39,6 @@ class AppServiceProvider extends ServiceProvider
             return $instance->setBlacklistEnabled((bool) config('jwt.blacklist_enabled'))
                             ->setPersistentClaims(config('jwt.persistent_claims'));
         });
+        \Iugu::setApiKey(env('IUGU_API_KEY'));//psr-0 integrando com a iugu
     }
 }
